a17cf870c220e74efd801e738b405ce4a6aab452
vahsimonian <simonianvah@gmail.com>
2023-08-30 22:44:19 +0700

    commit

---
+++ src/components/Inputs.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { UilSearch, UilLocationPoint } from '@iconscout/react-unicons'
+
+function Inputs() {
+  return (
+    <div className='flex flex-row justify-center my-6'>
+      <div></div>
+    </div>
+  )
+}
+
+export default Inputs
