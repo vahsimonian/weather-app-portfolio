55e003dcb151e7ce3239d3c4beb59e90d7ba52a8
vahsimonian <simonianvah@gmail.com>
2023-09-02 19:03:53 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className='mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400'>
       <TopButtons />
       <Inputs />
-
+      <h1></h1>
       <TimeAndLocation />
       <TemperatureAndDetails />
     </div>
--- src/components/TemperatureAndDetails.jsx
+++ src/components/TemperatureAndDetails.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 
 function TemperatureAndDetails() {
-  return <div>
-   
-  </div>
+  return (
+    <div>
+      <div className='flex items-center justify-center py-6 text-xl text-cyan-300'>
+        <p>clear</p>
+      </div>
+
+      <div className='flex flex-row items-center justify-between white py-3'>
+        <img
+          src={'https://cdn-icons-png.flaticon.com/512/831/831682.png'}
+          alt='sun'
+          className='w-20'
+        />
+        <p className='text-5xl'> 34° </p>
+        <div className='flex flex-col space-y-2'></div>
+      </div>
+    </div>
+  )
 }
 
 export default TemperatureAndDetails
